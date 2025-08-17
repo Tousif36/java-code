595adac42fb9ff900f69ce0114a6cdaaf5b0869f
Tousif Mulla <tousif323.tm@gmail.com>
2025-08-17 23:41:38 +0530

    calculator function is added

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,14 @@ class Calculate{
     public int add(int a, int b){
     return a+b; 
     }
+    public int sub(int a, int b){
+        return a-b;
+    }
 }
 public class Calculator{
     public static void main(String[] args) {
         Calculate calc = new Calculate();
-        System.out.println("Result is: " + calc.add(10,20));
+        System.out.println("Addition is: " + calc.add(10,20));
+        System.out.println("Substraction is: "+calc.sub(50, 20));
     }
 }
