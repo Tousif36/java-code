01a174693a90a51e35ce4197f7e38ab18128a2cd
Tousif Mulla <tousif323.tm@gmail.com>
2025-08-18 00:12:35 +0530

    multiplication of two number

--- Calculator.java
+++ Calculator.java
@@ -5,11 +5,15 @@ class Calculate{
     public int sub(int a, int b){
         return a-b;
     }
+    public int mult(int a, int b){
+        return a*b;
+    }
 }
 public class Calculator{
     public static void main(String[] args) {
         Calculate calc = new Calculate();
         System.out.println("Addition is: " + calc.add(10,20));
         System.out.println("Substraction is: "+calc.sub(50, 20));
+        System.out.println("Multiplication is: "+calc.mult(20, 5));
     }
 }
